c8b3bc2df6d0aaa27ebcd4c9138207dd60720947
ahsantariq792 <tariqahsan63@gmail.com>
2023-07-07 00:25:31 +0500

    api integration

--- basics-part-2/src/components/HomePage.vue
+++ basics-part-2/src/components/HomePage.vue
@@ -1,11 +1,26 @@
 <template>
     <h1>Home Page</h1>
+    <ul v-for="item in list" :key="item">
+        <li>{{ item.id }}</li>
+        <li> {{ item.email }}</li>
+    </ul>
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
-    name: 'HomePage'
+    name: 'HomePage',
+    data() {
+        return {
+            list: []
+        }
+    },
+    async mounted() {
+        let result = await axios.get("https://reqres.in/api/users?page=1");
+        console.log(result.data.data)
+        this.list = result.data.data
+    }
+
 }
 
 
--- basics-part-2/src/components/ProfilePage.vue
+++ basics-part-2/src/components/ProfilePage.vue
@@ -1,11 +1,31 @@
 <template>
     <h1>Profile Page</h1>
+    <input type="text" name="email" v-model="email" />
+    <input type="password" name="password" v-model="password" />
+    <button v-on:click="adddata()">Add Data</button>
 </template>
 
 <script>
 
+import axios from 'axios'
 export default {
-    name: 'ProfilePage'
+    name: 'ProfilePage',
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        adddata() {
+            console.log("email", this.email)
+            console.log("password", this.password)
+            axios.post("", {
+                email: this.email,
+                password: this.password
+            })
+        }
+    },
 }
 
 
